9ce1819ab9abf8e13476ec2a1b15be84d7daba43
writesandy <writesandy@gmail.com>
2018-03-06 17:31:49 -0600

    latest updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,10 @@ var words = ["chicken", "horse", "farmer", "haystack", "tractor"];
 
 //Random word selected
 
+var goodGuess = [];
+var badGuess = [];
+var guessLetters = [];
+
 
 var randomWord = words[Math.floor(Math.random()*words.length)];
 console.log(randomWord);
@@ -24,10 +28,32 @@ for (var i=0; i < randomWord.length; i++) {
 
 const compareWord = function () {
 document.addEventListener('keydown', (event) => {
+
+    //Get input value
+
     const keyName = event.key;
     console.log('keydown event\n\n' +keyName);
-    randomWord.search("keyName");
-    document.getElementById("badGuess").innerHTML = pos;
+
+    //check random word
+
+    var pos = randomWord.search("keyName");
+    console.log('pos:' +pos)
+
+    //Does character exist in randomWord
+
+    var character = keyName;
+
+    if (pos <= 0) {
+        console.log(pos);
+        push.character(goodGuess);
+        console.log('goodGuess' +goodGuess)
+        
+        //push goodGuess to array
+
+    }
+
+    // randomWord.search("keyName");
+    // document.getElementById("badGuess").innerHTML = pos;
 
 })
 
@@ -36,8 +62,4 @@ document.addEventListener('keydown', (event) => {
 
 compareWord();
 
-
-
-// Create function 
-// using event listener use keyName and find if randomWord includes keyName
 
